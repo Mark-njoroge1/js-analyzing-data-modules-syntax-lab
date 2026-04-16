5e63e12cbc4418453435879f7bb445ffebfbc4a3
Mark Njoroge <marknjoroge591@gmail.com>
2026-04-16 23:19:55 +0300

    add function of combine users

--- index.js
+++ index.js
@@ -4,3 +4,12 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+function combineUsers(...arrays){
+  const mergedUsers = [].concat(...arrays);
+  const today = new Date().toLocaleDateString("en-us");
+  return{
+    users: mergedUsers,
+    merge_date: today
+  };
+}
